5923099bfb401e5a507addd7b8f3f208a6423297
Kaushal Kumar <Kaushal.kr.kr@gmail.com>
2024-09-08 14:50:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+describe('Stylesheet Test', () => {
+  it('should include the stylesheet with href containing "styles.css"', () => {
+    // Visit the base URL
+    cy.visit(baseUrl);
+    
+    // Ensure the stylesheet link is present and loaded
+    cy.get('link[rel="stylesheet"][type="text/css"]').should('have.attr', 'href').and('include', 'styles.css');
+  });
+});
 
